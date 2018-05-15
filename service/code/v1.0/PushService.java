44b9a76b4b207f3cab4daa8fc5068b33d9a138bd
Hsu-chia-cheng <h1688i@gmail.com>
2018-05-15 11:21:53 +0800

    Add files via upload

--- service/code/v1.0/PushService.java
+++ service/code/v1.0/PushService.java
@@ -114,8 +114,8 @@ public class PushService extends Daemon {
 		String userId = User.id(this);
 		if(userId != null)
 		{
-			client = null;
-			client = new Client(userId);
+			if(client == null)
+				client = new Client(userId);
 			IO.LOG(className,"initClient","...");
 		}
 		else
