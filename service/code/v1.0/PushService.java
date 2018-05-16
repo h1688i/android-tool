66ac57ce01afb47993f5a0eaaa01e5b2d6bfc60d
Hsu-chia-cheng <h1688i@gmail.com>
2018-05-16 17:38:28 +0800

    Update PushService.java

--- service/code/v1.0/PushService.java
+++ service/code/v1.0/PushService.java
@@ -114,8 +114,9 @@ public class PushService extends Daemon {
 		String userId = User.id(this);
 		if(userId != null)
 		{
-			if(client == null)
-				client = new Client(userId);
+			if(client != null)
+				client = null;
+			client = new Client(userId);
 			IO.LOG(className,"initClient","...");
 		}
 		else
